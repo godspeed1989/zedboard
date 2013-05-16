a7e8ebe3f856ca911fc8f4f29c19e97baf1d2328
godspeed1989 <tongxinjichu@gmail.com>
2013-05-16 23:24:02 +0800

    update led driver user app the device must be reopened when to write.

--- led_drv/user_app/led_blink.c
+++ led_drv/user_app/led_blink.c
@@ -2,21 +2,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main()
+void do_write(unsigned char c)
 {
 	FILE* fp;
-	while(1)
+	fp = fopen("/proc/myled", "w");
+	if(fp == NULL)
 	{
-		fp = fopen("/proc/myled", "w");
-		if(fp == NULL) {
-			printf("Cannot open /proc/myled for write\n");
-			return -1;
-		}
-		fputs("0xa5\n", fp);
-		sleep(1);
-		fputs("0x5a\n", fp);
-		fclose(fp);
-		sleep(1);
+		printf("Cannot open /proc/myled for write\n");
+		return;
+	}
+	printf("0x%x\n", c);
+	fprintf(fp, "0x%x\n", c);
+	sleep(1);
+	fclose(fp);
+}
+
+int main()
+{
+	int i;
+	unsigned char c;
+	c = 0x55;
+	for(i=0; i<10; i++)
+	{
+		do_write(c);
+		c = ~c;
 	}
     return 0;
 }
